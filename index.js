c10f3f38414355348428b5fca2d2f53a20038a6d
Axaelon <87068399+Axaelon@users.noreply.github.com>
2021-08-23 13:34:26 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const Client = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+
+const prefix = "?";
+
+Client.on("ready", () => {
+    console.log("Bot opérationnel");
+});
+
+Client.on("messageCreate", message => {
+    if (message.author.bot) return;
+
+    if(message.content === prefix + "ping"){
+        message.channel.send("pong")
+    }
+    
+});
+
+
+Client.login(process.env.token);
